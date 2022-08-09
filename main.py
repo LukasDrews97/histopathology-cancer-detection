70283c01b260e90068a137cdd41115946eacf750
Lukas Drews <drews.lukas@yahoo.com>
2022-08-09 19:07:46 +0200

    changed learning rate, added image normalization, added training validation

--- main.py
+++ main.py
@@ -3,22 +3,21 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, random_split
 from torchvision import transforms
-from torchmetrics import functional as metrics
 from torchmetrics import MetricCollection, Accuracy, Precision, Recall, F1Score, AUROC
 import numpy as np
+import pandas as pd
 import random
 from tqdm import tqdm
 
 from dataset import HistopathologicImageDataset
-from models.model import Net
+from models.cnn_2 import Net
 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 dataset = HistopathologicImageDataset(labels_file="./data/train_labels.csv", img_dir="./data/train/", transform=transforms.Compose([
     transforms.PILToTensor(),
     transforms.ConvertImageDtype(dtype=torch.float32),
-    # TODO: normalize
-    #transforms.Normalize([0.5, 0.5, 0.5])#, [0.5, 0.5, 0.5]),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
 ]))
 
 
@@ -33,39 +32,83 @@ g.manual_seed(42)
 # Use worker_init_fn() and generator to preserve reproducibility
 train_set, test_set = random_split(dataset, [176_020, 44_005])
 train_loader = DataLoader(dataset=train_set, batch_size=64, shuffle=True, worker_init_fn=seed_worker, generator=g)
-test_loader = DataLoader(dataset=test_set, batch_size=64, shuffle=True, worker_init_fn=seed_worker, generator=g)
+test_loader = DataLoader(dataset=test_set, batch_size=64, shuffle=False, worker_init_fn=seed_worker, generator=g)
+
 
 net = Net(img_dim=[3, 96, 96])
 net.to(device)
 
 criterion = nn.BCELoss()
-optimizer = optim.Adam(net.parameters(), lr=1e-3)
+optimizer = optim.Adam(net.parameters(), lr=1e-4)
+
+metric_collection = MetricCollection([
+    Accuracy(),
+    Precision(),
+    Recall(),
+    F1Score(),
+    AUROC()
+])
 
 
 def train():
-    for epoch in tqdm(range(10)):
+    losses = []
+    accuracy = []
+    precision = []
+    recall = []
+    f1_score = []
+    auroc = []
+
+    for epoch in tqdm(range(1)):
         running_loss = 0.0
+        list_of_preds = []
+        list_of_labels = []
+
         for idx, data in tqdm(enumerate(train_loader, 0)):
             imgs, labels = data[0].to(device), data[1].to(torch.float32).to(device)
 
             optimizer.zero_grad()
 
-            outputs = net(imgs)
-            loss = criterion(outputs, labels)
+            preds = net(imgs)
+            loss = criterion(preds, labels)
             loss.backward()
             optimizer.step()
 
             running_loss += loss.item()
-            if idx % 200 == 199:       # print every 200 mini-batches
-                print(f'[{epoch + 1}, {idx + 1:5d}] loss: {running_loss / 200:.3f}')
-                running_loss = 0.0
+            list_of_preds.append(preds)
+            list_of_labels.append(labels)
 
-    PATH = "./first_model.pth"
+            if idx % 200 == 199: # calculate and print train metrics every 200 mini-batches
+                loss = running_loss / 200
+                losses.append(loss)
+
+                metrics = metric_collection(torch.cat(list_of_preds).cpu(), torch.cat(list_of_labels).cpu().to(torch.int8))
+                accuracy.append(metrics["Accuracy"].item())
+                precision.append(metrics["Precision"].item())
+                recall.append(metrics["Recall"].item())
+                f1_score.append(metrics["F1Score"].item())
+                auroc.append(metrics["AUROC"].item())
+
+                print(f'[{epoch + 1}, {idx + 1:5d}] loss: {loss:.3f}')
+                print(f'[{epoch + 1}, {idx + 1:5d}] accuracy: {accuracy[-1]:.3f} precision: {precision[-1]:.3f} recall: {recall[-1]:.3f} f1_score: {f1_score[-1]:.3f} auroc: {auroc[-1]:.3f}')
+                running_loss = 0.0
+                list_of_labels.clear()
+                list_of_preds.clear()
+
+    train_metrics = pd.Dataframe(
+        {'bce_loss': losses, 
+        'accuracy': accuracy, 
+        'precision': precision, 
+        'recall': recall,
+        'f1_score': f1_score,
+        'auroc': auroc
+        })
+    train_metrics.to_csv('train_metrics.csv')
+    PATH = "./second_model.pth"
     torch.save(net.state_dict(), PATH)
 
 
 def evaluate():
-    PATH = "./first_model.pth"
+    PATH = "./second_model.pth"
     net.load_state_dict(torch.load(PATH))
     net.cpu()
 
@@ -83,18 +126,18 @@ def evaluate():
 
     labels = torch.cat(list_of_labels)
     preds = torch.cat(list_of_preds)
-
-    metric_collection = MetricCollection([
-        Accuracy(),
-        Precision(),
-        Recall(),
-        F1Score(),
-        AUROC()
-    ])
-
-    print(metric_collection(preds, labels))
+    metrics = metric_collection(preds, labels)
+    test_metrics = pd.DataFrame({
+        'accuracy': metrics['Accuracy'].item(),
+        'precision': metrics['Precision'].item(),
+        'recall': metrics['Recall'].item(),
+        'f1_score': metrics['F1Score'].item(),
+        'auroc': metrics['AUROC'].item()
+        })
+    test_metrics.to_csv('test_metrics.csv')
+    print(metrics)
 
 
 if __name__=="__main__": 
-    #train()
-    evaluate()
+    train()
+    #evaluate()
